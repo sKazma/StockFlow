4f0d6bc5ef2efa5b0b36968ab35fb2ebd74eecc2
brehboyy <ousmane16diarra@gmail.com>
2016-05-30 16:53:27 +0200

    maj Ousmane

---
+++ src/Ventes.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Ventes {
+	static public ArrayList<Vente1> ventes=new ArrayList<Vente1>();
+	
+	static public void ajoutVente(Vente1 v){
+		if(v!=null){
+			ventes.add(v);
+		}
+		
+	}
+	static public void ajouterVente(Vente1 v){
+		ajoutVente(v);
+	}
+	
+	static public void listerVente(){
+		for (int i=0;i<ventes.size();i++){
+			System.out.println(ventes.get(i));
+		}
+	}
+	
+	static public void listerVenteClient(Client c){
+		for (int i=0;i<ventes.size();i++){
+			if (ventes.get(i).getClient().getMaReference()==c.getMaReference()){
+				System.out.println(ventes.get(i));
+			}
+		}
+	}
+}
